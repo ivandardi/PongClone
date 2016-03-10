e63ecfd368b82caea169c586b70e604d32d8ec97
ivandardi <ivy.dardi@gmail.com>
2016-03-10 19:06:35 -0300

    Improve build script

--- build.sh
+++ build.sh
@@ -1,18 +1,26 @@
-if [[ "$1" == "debug" ]]; then
-	BUILD_TYPE=Debug
+if [[ "$1" == "release" ]]; then
+	BUILD_TYPE=Release
 else
-	if [[ "$1" == "release" ]]; then
-		BUILD_TYPE=Release
-	else
-		BUILD_TYPE=""
-	fi
+	BUILD_TYPE=Debug
 fi
 
 echo "Building in ${BUILD_TYPE} Mode..."
 
-cd build/"$BUILD_TYPE"
+if [[ ! -d build/"$BUILD_TYPE" ]]; then
+	echo "Build directory doesn't exist. Creating it...'"
+	mkdir -p build/$BUILD_TYPE
+else
+	echo "Build directory already exists."
+fi
+
+echo "Calling CMake..."
+cd build/$BUILD_TYPE
 cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE ../..
-make VERBOSE=1
+make
 
-mv build/${BUILD_TYPE}/PongClone bin/${BUILD_TYPE}
+cd ../..
+if [[ ! -d bin/"$BUILD_TYPE" ]]; then
+	mkdir -p bin/$BUILD_TYPE
+fi
 
+mv -v -u build/${BUILD_TYPE}/PongClone bin/${BUILD_TYPE}
